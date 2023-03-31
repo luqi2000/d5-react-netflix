583d45f8257ed6aabf98ca5a588f0bd3028597bd
luqi2000 <luqmanmohammad09@gmail.com>
2023-03-31 16:01:58 +0100

    delete-node

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Header from "./components/Header";
-import MyFooter from "./components/MyFooter";
 
 import MyNavbar from "./components/MyNavbar";
 import Movies from "./components/Movies";
 import Movies2 from "./components/Movies2";
 import Movies3 from "./components/Movies3";
+import MyFooter from "./components/MyFooter";
 
 function App() {
   return (
